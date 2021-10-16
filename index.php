39d8c89d6c9d8f38ba3d5529fb427b44f53d81d5
YNGXZRIST <VVlKulagin@stud.kantiana.ru>
2021-10-16 11:25:33 +0200

    index.php : добавлен фильтр фильмов по возрасту

--- index.php
+++ index.php
@@ -1,38 +1,31 @@
 <?php
+
 require "movies.php";
-echo "inter your age: ";
 
-$age=readline("inter your age: ");
-	switch ($age)
-	{
+$age = readline("enter your age: ");
 
-	case ($age===0):
-			findMovies($movies,$age);
-			break;
-		case($age>0 && $age<12):
-			findMovies($movies,$age);
-		break;
-		case($age>0 && $age<=14):
-			findMovies($movies,$age);
-			break;
-		case($age>0 && $age<=16):
-			findMovies($movies,$age);
-			break;
-		case($age>0 && $age>=18):
-			findMovies($movies,$age);
-			break;
-		default:
-			echo "Enter currect age!";
-	}
-function findMovies(array $movies,int $age): void
+if (is_numeric($age) && $age >= 0)
 {
-	$i=1;
-foreach ($movies as $movie){
-	if ($movie['age_restriction']<=$age){
-		echo "{$i} . {$movie['title']}  ({$movie['release_year']}), {$movie['age_restriction']}+. Rating- {$movie['rating']}" ."\n";
-		$i++;
-	}
+	findMoviesByUserAge($movies, $age);
 }
+else
+{
+	echo "enter correct age";
+}
+function findMoviesByUserAge(array $movies, int $age): void
+{
+	$i = 1;
+
+	foreach ($movies as $movie)
+	{
+		if ($movie['age_restriction'] <= $age)
+		{
+			echo "{$i} . {$movie['title']}  ({$movie['release_year']}), {$movie['age_restriction']}+. Rating- {$movie['rating']}"
+				. "\n";
+			$i++;
+
+		}
+	}
 }
 
 
